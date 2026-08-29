9f5532fa727f8a13b673df4b6b038e09fe1206dc
brainage04 <thomasmcmah21@gmail.com>
2026-08-29 21:43:41 +1000

    Align Vencord sources with current lint rules

--- vencord/index.tsx
+++ vencord/index.tsx
@@ -1,3 +1,9 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2026 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 import { NavContextMenuPatchCallback } from "@api/ContextMenu";
 import { copyToClipboard } from "@utils/clipboard";
 import type { PluginDef } from "@utils/types";
